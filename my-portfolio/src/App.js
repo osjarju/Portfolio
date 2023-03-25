37441ddb7352d129d0104dc545ecea3ca76d0b5b
osjarju <115750838+osjarju@users.noreply.github.com>
2023-03-24 21:21:45 -0700

    navBar populated

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import './index.css';
+import Home from './routes/Home';
+import About from './routes/About';
+import Portfolio from './routes/Portfolio';
+import Contact from './routes/Contact';
+
+import { Route, Routes } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div><h1>This is my Portfolio</h1></div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ my-portfolio/src/components/Navbar.js
@@ -0,0 +1,31 @@
+import './styles.css';
+import { Link } from 'react-router-dom';
+import React from 'react';
+import Portfolio from '../routes/Portfolio';
+
+const Navbar = () => {
+    return (
+        <div className="header">
+            <Link to="/">
+                <h1>Portfolio</h1>
+            </Link>
+
+            <ul className='nav-menu'>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/portfolio">Portfolio</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
